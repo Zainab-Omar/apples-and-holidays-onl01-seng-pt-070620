2cabaf42886df7a003b7fadc5417cd2f0c4f1096
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-17 23:49:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,11 +16,10 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash.each do |weather, data|
-    #if wearher == :spring
+    if wearher == :spring
       data.each do |holiday, supplies|
-         binding.pry
         supplies << supply
-      #end
+      end
     end
   end
 end
