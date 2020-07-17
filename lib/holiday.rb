a18d34c53a165f1481581d9da2c4609102b71f81
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-17 22:28:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,8 +25,11 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  
-
+  holiday_hash.each do |weather, data|
+    if weather == :fall
+      data << holiday_name
+    end
+end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
