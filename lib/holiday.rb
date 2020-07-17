1a57392334f8ac06d78139802ce411b3cd08fbed
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-17 21:59:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -13,7 +13,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
    end
  end
 end
-end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
