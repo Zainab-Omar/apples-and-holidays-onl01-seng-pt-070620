13e7e27ccbb74f16081b93c73eb012dc851f6f39
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-17 23:53:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -14,6 +14,12 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
  end
 end
 
+#def add_supply_to_winter_holidays(holiday_hash, item)
+# holiday_hash[:winter].each do |holiday, decorations|
+  # decorations << item
+  #end
+#end
+
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].push(supply)
   #holiday_hash.each do |weather, data|
