517bc204bbcf312362e359f0ac5cf95fa7569672
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-17 22:55:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[:season]=holiday_name
 end
-end
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
