b145175ad567e850079d1f9e84f2d7ccd05f0528
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-18 00:45:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,8 +51,8 @@ holiday_hash.each do | season, holiday|
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+ holiday_hash.map do |season, holiday|
+   holiday.map do |holiday, item|
 
 end
 
